f63beec983b580cd051423b90956cabb46078ddb
topalex <topalex92@gmail.com>
2024-07-31 18:32:31 +0300

    avoid xft dependency

--- conanfile.py
+++ conanfile.py
@@ -157,7 +157,7 @@ class ImageMagickDelegates(ConanFile):
       if self.options.jxl:
         self.requires('libjxl/0.6.1')
         self.requires('brotli/1.1.0')
-        if self.options.simd:
+        if self.options.simd and self.settings.arch != 'wasm':
           self.requires('highway/1.0.3')
 
       if self.options.openmp and self.settings.arch != 'wasm' and self.settings.os != 'Windows':
@@ -170,13 +170,9 @@ class ImageMagickDelegates(ConanFile):
       if not self.options.conan or 'npm_config_external' in environ:
         return
 
-      if self.settings.arch != 'wasm' and self.options.fonts:
-        self.options['glib'].shared = False
-        self.options['glib'].fPIC = True
-
       if self.options.jpeg2000:
         self.options['jasper'].with_libjpeg = 'libjpeg-turbo'
-      
+
       if self.options.tiff:
         self.options['libtiff'].jpeg = 'libjpeg-turbo'
 
@@ -184,13 +180,12 @@ class ImageMagickDelegates(ConanFile):
         self.options['libraw'].with_jpeg = 'libjpeg-turbo'
 
       fonts_enabled = self.settings.arch != 'wasm' and self.options.fonts
-      if self.options.cairo and self.settings.arch != 'wasm':
+      if self.settings.arch != 'wasm' and (self.options.cairo or self.options.rsvg):
         self.options['cairo'].with_png = self.options.png
-        self.options['cairo'].with_glib = fonts_enabled
+        self.options['cairo'].with_glib = fonts_enabled or self.options.rsvg
         # There is no portable way to include xlib
         self.options['cairo'].with_xlib = False
         self.options['cairo'].with_xlib_xrender = False
-        self.options['cairo'].with_xcb = False
         self.options['cairo'].with_xcb = False
         self.options['cairo'].with_zlib = self.options.gzip
         self.options['cairo'].with_freetype = fonts_enabled
@@ -200,7 +195,12 @@ class ImageMagickDelegates(ConanFile):
         self.options['librsvg'].avif = False
         self.options['librsvg'].with_gdk_pixbuf = True
         self.options['librsvg']['pixbuf-loader'] = True
+        self.options['gdk-pixbuf'].with_libpng = self.options.png
+        self.options['gdk-pixbuf'].with_libtiff = self.options.tiff
         self.options['gdk-pixbuf'].with_libjpeg = 'libjpeg-turbo'
+        self.options['pango'].with_xft = False
+        self.options['pango'].with_freetype = fonts_enabled
+        self.options['pango'].with_fontconfig = fonts_enabled and self.settings.os != 'Windows'
 
       # While Emscripten supports SIMD, Node.js does not and cannot run the resulting WASM bundle
       # The performance gain is not very significant and it has a huge compatibility issue
